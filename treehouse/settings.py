22bfcbb5952c1e1b9ace2741e715ad11513a0a64
JonathanDelaney <glibbidydelaney@gmail.com>
2021-08-15 17:10:56 +0000

    deploy to heroku, migrate database

--- treehouse/settings.py
+++ treehouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from os import path
 if path.exists("env.py"):
@@ -122,13 +123,16 @@ WSGI_APPLICATION = 'treehouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://qulwibzsxfzvax:14ac0c86ebef7b6bd0ce8f305f73e2f967c809c158f4f6f160f7c71eb39039ff@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/dbvg072mbjaq5s')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
